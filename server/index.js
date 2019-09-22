650b903f70341b74d2697c2deb0b62861b1d9e4a
nikita-skobov <nskobov.websites@gmail.com>
2019-09-21 21:51:11 -0500

    prevents connections from changing game if already running

--- server/index.js
+++ server/index.js
@@ -84,18 +84,22 @@ io.on('connection', (socket) => {
         has_host = true
     }
 
-    if (positions.length === 0) {
+    if (positions.length === 0 && !game_running) {
         socket.disconnect()
         return null
     }
-    socket.color = positions.shift()
+    if (!game_running) {
+        socket.color = positions.shift()
+    }
     const my_player = {
         color: socket.color,
         join_order: 3 - positions.length,
         score: 0,
     }
-    players.push(my_player)
-    socket.broadcast.emit('newplayer', my_player)
+    if (!game_running) {
+        players.push(my_player)
+        socket.broadcast.emit('newplayer', my_player)
+    }
 
     socket.on('whoami', () => {
         const youare = {
@@ -107,152 +111,165 @@ io.on('connection', (socket) => {
         socket.emit('youare', youare)
     })
 
+
     socket.on('whois', () => {
-        if (socket.color) {
-            const output = []
-            players.forEach((player) => {
-                if (socket.color !== player.color) {
-                    output.push(player)
-                }
-            })
-            socket.emit('theyare', output)
-        }
+        const output = []
+        players.forEach((player) => {
+            if (socket.color !== player.color) {
+                output.push({ ...player, tiles: player.tiles.map(() => ' ')})
+            }
+        })
+        socket.emit('theyare', output)
     })
 
     socket.on('change_score', (new_value) => {
-        for (let i = 0; i < players.length; i += 1) {
-            if (players[i].color === socket.color) {
-                console.log(`player ${socket.color} changing score to: ${new_value}`)
-
-                players[i].score = new_value
-                socket.broadcast.emit('score_changed', socket.color, new_value)
-                break
+        if (game_running) {
+            for (let i = 0; i < players.length; i += 1) {
+                if (players[i].color === socket.color) {
+                    console.log(`player ${socket.color} changing score to: ${new_value}`)
+    
+                    players[i].score = new_value
+                    socket.broadcast.emit('score_changed', socket.color, new_value)
+                    break
+                }
             }
         }
     })
 
     socket.on('get_tiles', () => {
-        const color = socket.color
-        console.log(`${color} is asking for tiles`)
-        console.log(players)
-        let my_tiles = []
-        for (let i = 0; i < players.length; i += 1) {
-            if (players[i].color === color) {
-                let diff = 7 - players[i].tiles.length
-                let add_tiles = draw_n_tiles(diff)
-                add_tiles.forEach((tile) => {
-                    players[i].tiles.push(tile)
-                })
-                my_tiles = players[i].tiles
-                break
+        if (game_running) {
+            const color = socket.color
+            console.log(`${color} is asking for tiles`)
+            console.log(players)
+            let my_tiles = []
+            for (let i = 0; i < players.length; i += 1) {
+                if (players[i].color === color) {
+                    let diff = 7 - players[i].tiles.length
+                    let add_tiles = draw_n_tiles(diff)
+                    add_tiles.forEach((tile) => {
+                        players[i].tiles.push(tile)
+                    })
+                    my_tiles = players[i].tiles
+                    break
+                }
             }
+    
+            socket.emit('got_tiles', color, my_tiles)
+            let dummy_tiles = []
+            my_tiles.forEach((tile) => {
+                dummy_tiles.push(' ')
+            })
+            socket.broadcast.emit('got_tiles', color, dummy_tiles)
         }
-
-        socket.emit('got_tiles', color, my_tiles)
-        let dummy_tiles = []
-        my_tiles.forEach((tile) => {
-            dummy_tiles.push(' ')
-        })
-        socket.broadcast.emit('got_tiles', color, dummy_tiles)
     })
 
     socket.on('place_tile', (tile, pos) => {
-        if (socket.color) {
-            for (let i = 0; i < players.length; i += 1) {
-                if (players[i].color === socket.color && current_turn === socket.color) {
-                    console.log(`player: ${socket.color} is placing a '${tile}' on ${pos}`)
-                    const index_of_tile = players[i].tiles.indexOf(tile)
-                    if (index_of_tile !== -1) {
-                        if (!board[pos]) {
-                            // dont allow placing tiles on an existing tile
-                            players[i].tiles.splice(index_of_tile, 1)
-                            board[pos] = tile
-                            player_placed[pos] = socket.color
-                            socket.emit('tile_placed', socket.color, tile, pos)
-                            socket.broadcast.emit('tile_placed', socket.color, tile, pos)
+        if (game_running) {
+            if (socket.color) {
+                for (let i = 0; i < players.length; i += 1) {
+                    if (players[i].color === socket.color && current_turn === socket.color) {
+                        console.log(`player: ${socket.color} is placing a '${tile}' on ${pos}`)
+                        const index_of_tile = players[i].tiles.indexOf(tile)
+                        if (index_of_tile !== -1) {
+                            if (!board[pos]) {
+                                // dont allow placing tiles on an existing tile
+                                players[i].tiles.splice(index_of_tile, 1)
+                                board[pos] = tile
+                                player_placed[pos] = socket.color
+                                socket.emit('tile_placed', socket.color, tile, pos)
+                                socket.broadcast.emit('tile_placed', socket.color, tile, pos)
+                            }
                         }
+                        break
                     }
-                    break
                 }
             }
         }
     })
 
     socket.on('end_turn', () => {
-        if (socket.color) {
-            for (let i = 0; i < players.length; i += 1) {
-                if (players[i].color === socket.color && current_turn === socket.color) {
-                    let turn_index = turn_order.indexOf(socket.color)
-                    turn_index += 1
-                    if (turn_index >= players.length) {
-                        turn_index = 0
+        if (game_running) {
+            if (socket.color) {
+                for (let i = 0; i < players.length; i += 1) {
+                    if (players[i].color === socket.color && current_turn === socket.color) {
+                        let turn_index = turn_order.indexOf(socket.color)
+                        turn_index += 1
+                        if (turn_index >= players.length) {
+                            turn_index = 0
+                        }
+    
+                        let diff = 7 - players[i].tiles.length
+                        let add_tiles = draw_n_tiles(diff)
+                        add_tiles.forEach((tile) => {
+                            players[i].tiles.push(tile)
+                        })
+    
+                        socket.emit('got_tiles', socket.color, players[i].tiles)
+                        let dummy_tiles = []
+                        players[i].tiles.forEach((tile) => {
+                            dummy_tiles.push(' ')
+                        })
+                        socket.broadcast.emit('got_tiles', socket.color, dummy_tiles)
+    
+                        current_turn = turn_order[turn_index]
+                        socket.emit('next_turn', current_turn, tiles_remaining.length)
+                        socket.broadcast.emit('next_turn', current_turn, tiles_remaining.length)
+                        break
                     }
-
-                    let diff = 7 - players[i].tiles.length
-                    let add_tiles = draw_n_tiles(diff)
-                    add_tiles.forEach((tile) => {
-                        players[i].tiles.push(tile)
-                    })
-
-                    socket.emit('got_tiles', socket.color, players[i].tiles)
-                    let dummy_tiles = []
-                    players[i].tiles.forEach((tile) => {
-                        dummy_tiles.push(' ')
-                    })
-                    socket.broadcast.emit('got_tiles', socket.color, dummy_tiles)
-
-                    current_turn = turn_order[turn_index]
-                    socket.emit('next_turn', current_turn, tiles_remaining.length)
-                    socket.broadcast.emit('next_turn', current_turn, tiles_remaining.length)
-                    break
                 }
             }
         }
     })
 
     socket.on('take_tile', (pos) => {
-        if (socket.color) {
-            for (let i = 0; i < players.length; i += 1) {
-                if (players[i].color === socket.color) {
-                    if (current_turn === socket.color && board[pos]) {
-                        const placed_by = player_placed[pos]
-                        if (placed_by === socket.color) {
-                            const tile = board[pos]
-                            delete board[pos]
-                            delete placed_by[pos]
-                            players[i].tiles.push(tile)
-                            socket.emit('tile_taken', socket.color, tile, pos)
-                            socket.broadcast.emit('tile_taken', socket.color, ' ', pos)
+        if (game_running) {
+            if (socket.color) {
+                for (let i = 0; i < players.length; i += 1) {
+                    if (players[i].color === socket.color) {
+                        if (current_turn === socket.color && board[pos]) {
+                            const placed_by = player_placed[pos]
+                            if (placed_by === socket.color) {
+                                const tile = board[pos]
+                                delete board[pos]
+                                delete placed_by[pos]
+                                players[i].tiles.push(tile)
+                                socket.emit('tile_taken', socket.color, tile, pos)
+                                socket.broadcast.emit('tile_taken', socket.color, ' ', pos)
+                            }
                         }
+                        break
                     }
-                    break
                 }
             }
         }
     })
 
     socket.on('start_game', () => {
-        if (socket.is_host) {
-            let temp = []
-            for (let i = 0; i < players.length; i += 1) {
-                temp.push(players[i].color)
+        if (!game_running) {
+            if (socket.is_host) {
+                let temp = []
+                for (let i = 0; i < players.length; i += 1) {
+                    temp.push(players[i].color)
+                }
+                temp = shuffle(temp)
+    
+                for (let i = 0; i < players.length; i += 1) {
+                    players[i].tiles = draw_n_tiles(7)
+                }
+    
+                temp.forEach((col) => {
+                    turn_order.push(col)
+                })
+    
+                game_running = true
+                current_turn = temp[0]
+                socket.emit('game_started', temp, tiles_remaining.length)
+                socket.broadcast.emit('game_started', temp, tiles_remaining.length)
+                console.log('STARTING GAME WOOHOOO!')
             }
-            temp = shuffle(temp)
-
-            for (let i = 0; i < players.length; i += 1) {
-                players[i].tiles = draw_n_tiles(7)
-            }
-
-            temp.forEach((col) => {
-                turn_order.push(col)
-            })
-
-            current_turn = temp[0]
-            socket.emit('game_started', temp, tiles_remaining.length)
-            socket.broadcast.emit('game_started', temp, tiles_remaining.length)
-            console.log('STARTING GAME WOOHOOO!')
         }
     })
+
 })
 
 http.listen({
